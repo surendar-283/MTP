61763a287b330b00ae6f72dc3dd201ba6a74e352
Surendar <surendarmohan283@gmail.com>
2025-09-17 15:10:56 +0530

    sample code for testing custom heap blocks

--- custom-heap-block/mywrap.ml
+++ custom-heap-block/mywrap.ml
@@ -3,6 +3,6 @@ type myint
 external make_myint : int -> myint = "ml_make_myint"
 
 let () =
-  let _ = make_myint 100 in
+  let _ = make_myint 10 in
   Gc.compact ();  (* force garbage collection *)
   print_endline "Done";;
