e01c8da8bd2e67a7c76311c0aec2d704965bb56e
Surendar <surendarmohan283@gmail.com>
2025-09-17 15:09:49 +0530

    sample code for testing custom heap blocks

--- custom-heap-block/mywrap.c
+++ custom-heap-block/mywrap.c
@@ -6,7 +6,7 @@
 
 /* Finalizer: runs when GC frees the block */
 static void finalize_myint(value v) {
-  int x = *((int *) Data_custom_val(v));
+  int x = *((int *) Data_custom_val(v)); //Data_custom_val returns a void* to the raw value
   printf("Finalizing custom int %d\n", x);
 }
 
@@ -22,9 +22,14 @@ static struct custom_operations myint_ops = {
 };
 
 /* Allocate a custom block holding one int */
+// CAMLprim is a macro used by OCaml to mark a C function that will be called from OCaml.
 CAMLprim value ml_make_myint(value vx) {
-  CAMLparam1(vx);
+  CAMLparam1(vx); //This macro registers the OCaml parameter vx with the GC.
+
+  //caml_alloc_custom allocates a custom block on the OCaml heap.
+  //0->memory used for minor gc and 1->memory used for major gc
   value v = caml_alloc_custom(&myint_ops, sizeof(int), 0, 1);
   *((int *) Data_custom_val(v)) = Int_val(vx);
   CAMLreturn(v);
 }
+
