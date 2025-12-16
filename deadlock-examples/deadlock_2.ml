26eccacba6cbe302ee5103f6e62c3b62934e86d6
surendar-283 <84138292+surendar-283@users.noreply.github.com>
2025-12-16 13:13:31 +0530

    Refactor deadlock example to use task references

--- deadlock-examples/deadlock_2.ml
+++ deadlock-examples/deadlock_2.ml
@@ -1,46 +1,30 @@
 open Domainslib
 
-module T = Task
-
-let spin () =
-  for _ = 1 to 50_000_000 do
-    ()
-  done
-
 let () =
+  let pool = Task.setup_pool ~num_domains:1 () in
 
-  let m1 = Mutex.create () in
-  let m2 = Mutex.create () in
+  Task.run pool (fun _ ->
+    let a_ref = ref None in
+    let b_ref = ref None in
 
-  let pool = T.setup_pool ~num_domains:2 () in
-
-  T.run pool (fun _ ->
-    let t1 =
-      T.async pool (fun _ ->
-        Mutex.lock m1;
-        Printf.printf "Task 1: locked m1\n%!";
-        spin (); 
-        Mutex.lock m2;
-        Printf.printf "Task 1: locked m2\n%!";
-        Mutex.unlock m2;
-        Mutex.unlock m1
+    let a =
+      Task.async pool (fun _ ->
+        Printf.printf "Task A waiting for Task B\n%!";
+        ignore (Task.await pool (Option.get !b_ref))
       )
     in
 
-    let t2 =
-      T.async pool (fun _ ->
-        Mutex.lock m2;
-        Printf.printf "Task 2: locked m2\n%!";
-        spin ();
-        Mutex.lock m1;
-        Printf.printf "Task 2: locked m1\n%!";
-        Mutex.unlock m1;
-        Mutex.unlock m2
+    let b =
+      Task.async pool (fun _ ->
+        Printf.printf "Task B waiting for Task A\n%!";
+        ignore (Task.await pool (Option.get !a_ref))
       )
     in
-    
-    T.await pool t1;
-    T.await pool t2;
+
+    a_ref := Some a;
+    b_ref := Some b;
+
+    ignore (Task.await pool a)
   );
 
-  T.teardown_pool pool
+  Task.teardown_pool pool
